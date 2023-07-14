4ec65e76e5a4c959c07a5397e66aef11390365e8
Nick Marks <nmarks99@gmail.com>
2023-07-14 13:08:12 -0500

    x,y,theta instead of theta,x,y for Pose2D

--- src/rigid2d.rs
+++ src/rigid2d.rs
@@ -1,5 +1,4 @@
 /// rigid2D: 2D rigid body motion library
-
 use num_traits::Float;
 use std::fmt::Display;
 use std::ops;
@@ -11,7 +10,7 @@ pub struct Vector2D<T: Float> {
     pub x: T,
 
     /// y coordinate
-    pub y: T
+    pub y: T,
 }
 
 /// Implements the operation: Vector2D + Vector2D
@@ -23,86 +22,84 @@ impl<T: Float> ops::Add<Vector2D<T>> for Vector2D<T> {
 }
 
 /// Implements the Displayy trait for Vector2D
-impl<T:Float + Display> Display for Vector2D<T> {
+impl<T: Float + Display> Display for Vector2D<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      write!(f, "[{}, {}]", self.x, self.y)
+        write!(f, "[{}, {}]", self.x, self.y)
     }
 }
 
 impl<T: Float> Vector2D<T> {
-    
     /// constructs a Vector2D from floats (x,y)
     pub fn new(x: T, y: T) -> Self {
-        Vector2D {x, y}
+        Vector2D { x, y }
     }
 
     /// Creates a Vector2D from polar coordinates
     pub fn from_polar(r: T, phi: T) -> Self {
-        Vector2D { x: r*phi.cos() , y: r*phi.sin() }
+        Vector2D {
+            x: r * phi.cos(),
+            y: r * phi.sin(),
+        }
     }
-    
+
     /// computes the L2 norm of the 2D vector
     pub fn magnitude(&self) -> T {
         let x_sqr = self.x.powf(T::from(2.0).unwrap());
         let y_sqr = self.y.powf(T::from(2.0).unwrap());
         (x_sqr + y_sqr).sqrt()
     }
-    
+
     /// computes the dot product of the vector with another vector
     pub fn dot(&self, rhs: &Vector2D<T>) -> T {
         (self.x * rhs.x) + (self.y * rhs.y)
     }
-    
+
     /// computes the angle between two vectors
     pub fn angle(&self, rhs: &Vector2D<T>) -> T {
         let dot_prod = self.dot(rhs);
         let prod_of_msg = self.magnitude() * rhs.magnitude();
         return (dot_prod / prod_of_msg).acos();
     }
-    
+
     /// normalizes the vector
     pub fn normalize(&self) -> Self {
         let v_norm: Self;
         if self.x.is_zero() || self.y.is_zero() {
             v_norm = Vector2D::new(T::zero(), T::zero());
-        }
-        else {
+        } else {
             let mag = self.magnitude();
-            v_norm = Vector2D::new(self.x/mag, self.y/mag);
+            v_norm = Vector2D::new(self.x / mag, self.y / mag);
         }
         return v_norm;
     }
 }
 
-
 /// A 2-dimensional pose
 #[derive(Debug)]
 pub struct Pose2D<T: Float> {
-    /// Rotation in radians
-    pub theta: T,
-
     /// x position
     pub x: T,
 
     /// y position
-    pub y: T
+    pub y: T,
+
+    /// Rotation in radians
+    pub theta: T,
 }
 
-impl<T:Float> Pose2D<T> {
-
+impl<T: Float> Pose2D<T> {
     /// constructs a new Pose2D object from (theta,x,y) which are floats
-    pub fn new(theta: T, x: T, y: T)  -> Self {
-        Pose2D {theta, x, y}
+    pub fn new(x: T, y: T, theta: T) -> Self {
+        Pose2D { x, y, theta }
     }
 }
 
 /// Implments the Display trait
-impl<T:Float + Display> Display for Pose2D<T> {
+impl<T: Float + Display> Display for Pose2D<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      write!(f, "deg:{} x:{} y:{}", self.theta, self.x, self.y)
+        write!(f, "deg:{} x:{} y:{}", self.theta, self.x, self.y)
     }
 }
-
 
 /// A 2-dimensional Twist
 #[derive(Debug)]
@@ -113,42 +110,46 @@ pub struct Twist2D<T: Float> {
     /// x velocity
     pub xdot: T,
 
-    /// y 
-    pub ydot: T
+    /// y
+    pub ydot: T,
 }
 
 impl<T: Float> Twist2D<T> {
-
     /// constructs as new Twist2D object from theta, x, and y velocities
     pub fn new(thetadot: T, xdot: T, ydot: T) -> Self {
-        Twist2D {thetadot, xdot, ydot}
+        Twist2D {
+            thetadot,
+            xdot,
+            ydot,
+        }
     }
 }
 
 /// Implements the Display trait
-impl<T:Float + Display> Display for Twist2D<T> {
+impl<T: Float + Display> Display for Twist2D<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      write!(f, "[{}, {}, {}]", self.thetadot, self.xdot, self.ydot)
+        write!(f, "[{}, {}, {}]", self.thetadot, self.xdot, self.ydot)
     }
 }
 
 /// A 2-dimensional rigid body transformation
 pub struct Transform2D<T: Float> {
-
     /// rotational component of the transform in radians
     angle: T,
 
     /// translational component of the transform
-    p_vec: Vector2D<T>
+    p_vec: Vector2D<T>,
 }
 
 impl<T: Float> Transform2D<T> {
-    
     /// contructs a new Transform2D from a translation and rotation
     pub fn new(trans: Vector2D<T>, angle: T) -> Self {
-        Transform2D {p_vec: trans, angle} 
+        Transform2D {
+            p_vec: trans,
+            angle,
+        }
     }
-    
+
     /// returns the rotatational component of the transform
     pub fn rotation(&self) -> T {
         self.angle
@@ -159,6 +160,3 @@ impl<T: Float> Transform2D<T> {
         self.p_vec
     }
 }
-
-
-
--- tests/pose2d_tests.rs
+++ tests/pose2d_tests.rs
@@ -2,8 +2,8 @@ use diff_drive::rigid2d::Pose2D;
 
 #[test]
 fn pose2d_new() {
-    let p = Pose2D::new(90.0,2.0,3.0);
-    assert_eq!(p.theta, 90.0);
+    let p = Pose2D::new(2.0, 3.0, 90.0);
     assert_eq!(p.x, 2.0);
     assert_eq!(p.y, 3.0);
+    assert_eq!(p.theta, 90.0);
 }
